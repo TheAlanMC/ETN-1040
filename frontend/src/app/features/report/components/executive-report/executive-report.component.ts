3318d9f01569ad04f7e2da3052e6e8a673344b4d
Chris Apaza <chrisalanapazaa@gmail.com>
2024-05-28 00:32:16 -0400

    Added PDF motor to generate PDF

--- frontend/src/app/features/report/components/executive-report/executive-report.component.ts
+++ frontend/src/app/features/report/components/executive-report/executive-report.component.ts
@@ -5,6 +5,11 @@ import {FileService} from "../../../../core/services/file.service";
 import {MessageService} from "primeng/api";
 import {ExecutiveReportDto} from "../../models/executive-report.dto";
 import {ProjectReportDto} from "../../models/project-report.dto";
+import {PdfGenerationService} from "../../../../core/services/pdf-generation.service";
+import {FileDto} from "../../../../core/models/file.dto";
+import {Directory, Filesystem} from "@capacitor/filesystem";
+import {FileOpener} from "@capacitor-community/file-opener";
+import {UtilService} from "../../../../core/services/util.service";
 
 @Component({
     selector: 'app-executive-report',
@@ -14,7 +19,11 @@ import {ProjectReportDto} from "../../models/project-report.dto";
 })
 export class ExecutiveReportComponent implements OnInit {
 
+    isLoading: boolean = false;
+
     executiveReport: ExecutiveReportDto | null = null;
+
+    file: FileDto | null = null;
 
 
     dateFromControl = new FormControl('',
@@ -30,11 +39,16 @@ export class ExecutiveReportComponent implements OnInit {
 
     expandedRows = {};
 
+    isMobile: boolean = false;
+
     constructor(
         private reportService: ReportService,
         private fileService: FileService,
-        private messageService: MessageService
+        private messageService: MessageService,
+        private pdfGenerationService: PdfGenerationService,
+        private utilService: UtilService,
     ) {
+        this.isMobile = this.utilService.checkIfMobile();
     }
 
     ngOnInit() {
@@ -44,8 +58,10 @@ export class ExecutiveReportComponent implements OnInit {
 
     public getData() {
         this.isDataLoading = true;
-        this.reportService.getExecutiveReport((new Date(this.dateFromControl.value!)).toISOString(),
-            (new Date(this.dateToControl.value!)).toISOString(),).subscribe({
+        this.reportService.getExecutiveReport(
+            (new Date(this.dateFromControl.value!)).toISOString(),
+            (new Date(this.dateToControl.value!)).toISOString(),
+            ).subscribe({
             next: (data) => {
                 this.executiveReport = data.data;
                 this.isDataLoading = false;
@@ -64,7 +80,98 @@ export class ExecutiveReportComponent implements OnInit {
     }
 
     public onExportPdf() {
-        console.log('Exporting PDF');
+        this.isLoading = true;
+        this.pdfGenerationService.generatePdf('executiveReportContent', 'Reporte Ejecutivo.pdf').subscribe(
+            {
+                next: (pdf) => {
+                    this.upLoadFile(pdf);
+                },
+                error: (error) => {
+                    console.error(error);
+                    this.isLoading = false;
+                    this.messageService.add({severity: 'error', summary: 'Error', detail: error.error.message});
+                }
+            }
+        );
+    }
+
+    public upLoadFile(file: File) {
+        this.isLoading = true;
+        this.fileService.uploadFile(file).subscribe({
+            next: (response) => {
+                this.file = response.data;
+                this.saveReport();
+            },
+            error: (error) => {
+                console.error(error);
+                this.isLoading = false;
+                this.messageService.add({severity: 'error', summary: 'Error', detail: error.error.message});
+            }
+        });
+    }
+
+    public saveReport() {
+        this.reportService.uploadReport(
+            (new Date(this.dateFromControl.value!)).toISOString(),
+            (new Date(this.dateToControl.value!)).toISOString(),
+            'EJECUTIVO',
+            this.file!.fileId,
+            this.file!.fileName,
+            this.file!.contentType,
+            this.file!.fileSize,
+        ).subscribe({
+            next: (response) => {
+                this.downloadReport();
+                this.isLoading = false;
+                this.messageService.add({severity: 'success', summary: 'Éxito', detail: response.message});
+            },
+            error: (error) => {
+                console.error(error);
+                this.isLoading = false;
+                this.messageService.add({severity: 'error', summary: 'Error', detail: error.error.message});
+            }
+        });
+    }
+
+    public downloadReport() {
+        this.fileService.getFile(this.file!.fileId).subscribe({
+            next: (data) => {
+                const blob = new Blob([data],
+                    {type: data.type});
+                if (!this.isMobile) {
+                    const url = URL.createObjectURL(blob);
+                    const a = document.createElement('a');
+                    a.href = url;
+                    a.download = this.file!.fileName;
+                    document.body.appendChild(a);
+                    a.click();
+                    URL.revokeObjectURL(url);
+                    document.body.removeChild(a);
+                } else {
+                    const reader = new FileReader();
+                    reader.onloadend = async () => {
+                        const base64Data = reader.result as string;
+                        const savedFile = await Filesystem.writeFile({
+                            path: this.file!.fileName,
+                            data: base64Data,
+                            directory: Directory.Documents,
+                        });
+                        const fileOpenerOptions = {
+                            filePath: savedFile.uri,
+                            contentType: this.file!.contentType,
+                            openWithDefault: true,
+                        };
+                        await FileOpener.open(fileOpenerOptions);
+                    };
+                    reader.readAsDataURL(blob);
+                }
+            },
+            error: (error) => {
+                console.error(error);
+                this.isLoading = false;
+                this.messageService.add({severity: 'error', summary: 'Error', detail: error.error.message});
+            }
+        });
     }
 
     public onClear() {
@@ -81,4 +188,6 @@ export class ExecutiveReportComponent implements OnInit {
     collapseAll() {
         this.expandedRows = {};
     }
+
+
 }
