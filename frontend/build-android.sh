4a41d38ce64da06d10e0ad026562f75bb9aa33fc
Chris Apaza <chrisalanapazaa@gmail.com>
2024-10-07 04:40:21 -0400

    Added .gitkeep

--- frontend/build-android.sh
+++ frontend/build-android.sh
@@ -11,7 +11,7 @@ if [ -z "$version_name" ]; then
     version_name="1.0.0"
 fi
 
-# Package the angular application, with the name frontend
+# Package the Angular App:
 ng build --configuration android --output-path dist/frontend
 
 # Generate the Assets:
@@ -19,10 +19,14 @@ npx capacitor-assets generate
 
 npx cap sync android
 cd android
+# Generate the APK file
 ./gradlew assembleRelease -PversionCode=1 -PversionName=$version_name
+# Generate the AAB file
+./gradlew bundleRelease -PversionCode=1 -PversionName=$version_name
 cd ..
-cp android/app/build/outputs/apk/release/app-release.apk android/app/build/outputs/apk/release/app-release-$version_name.apk
+cp android/app/build/outputs/bundle/release/app-release.aab laboratorio_multimedia_v$version_name.aab
 cp android/app/build/outputs/apk/release/app-release.apk laboratorio_multimedia_v$version_name.apk
 
-echo "APK built successfully!"
-echo "APK file: laboratorio_multimedia_v$version_name.apk"
+echo "APK and AAB files generated successfully"
+echo "APK: laboratorio_multimedia_v$version_name.apk"
+echo "AAB: laboratorio_multimedia_v$version_name.aab"
